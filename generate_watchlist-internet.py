44804a4717367797a5002c83cb60d6a23b078649
Simon <SaimenSays@users.noreply.github.com>
2026-02-08 22:31:33 +0100

    Create generate_watchlist-internet.py

---
+++ generate_watchlist-internet.py
@@ -0,0 +1,18 @@
+import csv
+import requests
+
+url = "https://www.watchlist-internet.at/liste-betruegerischer-shops/csv/"
+response = requests.get(url)
+response.encoding = "utf-8"
+
+domains = set()
+
+for row in csv.reader(response.text.splitlines(), delimiter=';'):
+    if row and row[0] != "Domain":
+        domain = row[0].strip().lower()
+        if domain:
+            domains.add(domain)
+
+with open("blocklist.txt", "w") as f:
+    for d in sorted(domains):
+        f.write(d + "\n")
