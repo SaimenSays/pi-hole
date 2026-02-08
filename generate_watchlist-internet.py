da40310eb396026811ea5d400f1aba8546180a4e
Simon <SaimenSays@users.noreply.github.com>
2026-02-08 22:44:11 +0100

    Update generate_watchlist-internet.py
    Corrected filename

--- generate_watchlist-internet.py
+++ generate_watchlist-internet.py
@@ -13,6 +13,6 @@ for row in csv.reader(response.text.splitlines(), delimiter=';'):
         if domain:
             domains.add(domain)
 
-with open("watchlist-internet", "w") as f:
+with open("watchlist-internet.txt", "w") as f:
     for d in sorted(domains):
         f.write(d + "\n")
